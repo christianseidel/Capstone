1fb29ff17b6a3ef638e1d5d977ed6bf5a44ffccd
Christian Seidel <github@parameter5.de>
2023-01-03 12:35:48 +0100

    fixed

--- backend/src/main/java/de/neuefische/smartcount/Users/UserLoginData.java
+++ backend/src/main/java/de/neuefische/smartcount/Users/UserLoginData.java
@@ -2,9 +2,11 @@ package de.neuefische.smartcount.Users;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginData {
 
     private String username;
